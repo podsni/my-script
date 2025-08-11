5f563ad708ce5c49787c9f3ed01b7ce8a27c49a6
LocalSo <localso@proton.me>
2025-08-11 14:32:01 +0700

    Update omakub.sh

--- script/setupOS/omakub.sh
+++ script/setupOS/omakub.sh
@@ -2,11 +2,11 @@
 #Omakub is only tested on fresh installations of Ubuntu 24.04 and 25.04. If you already have an installation, use a different version, or even a different distribution, you'll be on your own.)
 set -euo pipefail
 
-URL="https://raw.githubusercontent.com/basecamp/omakub/stable/boot.sh"
+URL="https://omakub.org/install"
 
 # Simpan sementara file sebelum eval (supaya bisa diperiksa kalau mau)
 TMP_FILE=$(mktemp)
-wget -qO "$TMP_FILE" "$URL"
+wget -qO- "$TMP_FILE" "$URL"
 
 echo "[INFO] Script berhasil diunduh ke $TMP_FILE"
 echo "[INFO] Menjalankan script..."
